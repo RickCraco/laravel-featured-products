b385a5afbb561938a2b38f8cb4fe6d29c9c817d4
RickCraco <riccardocracolici43@gmail.com>
2024-03-13 12:29:33 +0100

    added products on the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,10 @@
 @section('content')
 <main>
     <h1>Home page</h1>
+
+    @foreach ($products as $product)
+        <h2>{{ $product->name }}, {{$product->category->name}}</h2>
+    @endforeach
 </main>
 
 @endsection
