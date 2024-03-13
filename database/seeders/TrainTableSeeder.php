8f973ecdc39ec677b74be93a8282fed5a3534a8d
Riccardo Cracolici <140084513+RickCraco@users.noreply.github.com>
2024-03-13 10:57:57 +0100

    Initial commit

---
+++ database/seeders/TrainTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Generator as Faker;
+use Illuminate\Database\Seeder;
+
+class TrainTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 0; $i < 10; $i++) {
+            $newTrain = new Train();
+            $newTrain->company = $faker->word();
+            $newTrain->departure_station = $faker->word();
+            $newTrain->arrival_station = $faker->word();
+            $newTrain->departure_time = $faker->time();
+            $newTrain->arrival_time = $faker->time();
+            $newTrain->train_code = $faker->bothify('####-????');
+            $newTrain->number_of_carriages = $faker->numberBetween(1, 10);
+            $newTrain->in_time = $faker->boolean();
+            $newTrain->cancelled = $faker->boolean();
+            $newTrain->save();
+        }
+    }
+}
