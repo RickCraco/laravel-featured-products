c9888de0b08cf4bc13e776b2b2289700ae749675
RickCraco <riccardocracolici43@gmail.com>
2024-03-13 13:34:54 +0100

    added category filter to products in home page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,13 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Category;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::all();
-        return view('home', compact('products'));
+        $products = Product::query();
+
+        if($request->input('category')){
+            $products->whereHas('category', function($query) use ($request){
+                $query->where('name', $request->input('category'));
+            });
+        }
+
+        $products->orderBy('featured', 'desc');
+        $products->orderBy('name', 'asc');
+
+        $products = $products->get();
+        $categories = Category::all();
+
+        return view('home', compact('products', 'categories'));
     }
 
     public function show(Product $product)
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,10 +8,21 @@
     <div class="container">
         <h1>Our products</h1>
 
+        <form action="/" method="GET">
+            <select name="category" id="category">
+                <option value="">All</option>
+                @foreach ($categories as $category)
+                    <option value="{{ $category->name }}">{{ $category->name }}</option>
+                @endforeach
+            </select>
+            <button type="submit">Filter</button>
+        </form>
+
         <div class="row">
             @foreach ($products as $product)
                 <div class="card col-3 mx-2">
                     <div class="card-body">
+                        <span>{{$product->featured}}</span>
                         <h5 class="card-title">{{ $product->name }}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">{{ $product->category->name }}</h6>
                         <p class="card-text">{{ $product->description }}</p>
