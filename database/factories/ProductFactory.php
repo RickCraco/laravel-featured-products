5108c0fa9f1f8ad64b6c0c392c16f19d4e569034
RickCraco <riccardocracolici43@gmail.com>
2024-03-18 17:00:13 +0100

    fixed factory problem

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Product;
 use App\Models\Category;
 use Faker\Generator as Faker;
@@ -18,17 +19,15 @@ class ProductFactory extends Factory
     {
         $categories = Category::pluck('id');
 
-        for ($i = 0; $i < 10; $i++) {
-            return [
-                'name' => $this->faker->sentence(),
-                'description' => $this->faker->text(),
-                'image' => $this->faker->imageUrl(640, 480),
-                'code_ean' => $this->faker->ean13(),
-                'price' => $this->faker->randomFloat(2, 0, 1000),
-                'featured' => $this->faker->boolean(),
-                'category_id' => $this->faker->randomElement($categories),
-                'slug' => Str::slug($this->faker->name()),
-            ];
-        }
+        return [
+            'name' => $name = $this->faker->name(),
+            'description' => $this->faker->text(),
+            'image' => $this->faker->imageUrl(640, 480),
+            'code_ean' => $this->faker->ean13(),
+            'price' => $this->faker->randomFloat(2, 0, 1000),
+            'featured' => $this->faker->boolean(),
+            'category_id' => $this->faker->randomElement($categories),
+            'slug' => Str::slug($name),
+        ];
     }
 }
